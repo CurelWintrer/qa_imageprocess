ee673d2ad7a1b564bf8e7bf8644f0def79489f60
WinterCruel <78720290+CurelWintrer@users.noreply.github.com>
2025-08-14 15:40:17 +0800

    图片详情

---
+++ lib/MyWidget/image_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImageDetail extends StatefulWidget {
+  const ImageDetail({super.key});
+
+  @override
+  State<ImageDetail> createState() => _ImageDetailState();
+}
+
+class _ImageDetailState extends State<ImageDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
